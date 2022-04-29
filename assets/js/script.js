ed4cc3645408facebdbadac4268f19e8617e2bb4
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-04-29 10:11:14 -0500

    added new project images

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@ const hamburgerIconEl = document.querySelector(".hamburger")
 const modalEl = document.querySelector(".modal")
 const closeIconEl = document.querySelector(".close-icon")
 
-const landingPagesContainerEl = $(".landingpages-projects-container")
+const frontEndProjectsContainerEl = $(".front-end-projects-container")
+const backeEndProjectsContainerEl = $(".back-end-projects-container")
 const musicProjectsContainerEl = $(".music-projects-container")
-const javascriptProjectsContainerEl = $(".javascript-projects-container")
 
 
 const showMenu = () => {    
@@ -22,37 +22,40 @@ $(".modal-anchor").on("click", () => {
 
 })
 
-$(".music-projects").on("click", () => {
-    landingPagesContainerEl.addClass("hidden")
-    landingPagesContainerEl.removeClass("grid-container")
+$(".back-end").on("click", () => {
+    frontEndProjectsContainerEl.addClass("hidden")
+    frontEndProjectsContainerEl.removeClass("grid-container")
 
-    javascriptProjectsContainerEl.addClass("hidden")
-    javascriptProjectsContainerEl.removeClass("grid-container")
+    musicProjectsContainerEl.addClass("hidden")
+    musicProjectsContainerEl.removeClass("grid-container")
+
+    backeEndProjectsContainerEl.removeClass("hidden")
+    backeEndProjectsContainerEl.addClass("grid-container")
+
+})
+$(".front-end").on("click", () => {
+    backeEndProjectsContainerEl.addClass("hidden")
+    backeEndProjectsContainerEl.removeClass("grid-container")
+
+    musicProjectsContainerEl.addClass("hidden")
+    musicProjectsContainerEl.removeClass("grid-container")
+
+    frontEndProjectsContainerEl.removeClass("hidden")
+    frontEndProjectsContainerEl.addClass("grid-container")
+})
+$(".music-projects").on("click", () => {
+    backeEndProjectsContainerEl.addClass("hidden")
+    backeEndProjectsContainerEl.removeClass("grid-container")
+
+    frontEndProjectsContainerEl.addClass("hidden")
+    frontEndProjectsContainerEl.removeClass("grid-container")
 
     musicProjectsContainerEl.removeClass("hidden")
     musicProjectsContainerEl.addClass("grid-container")
-
-})
-$(".landing-pages").on("click", () => {
-    musicProjectsContainerEl.addClass("hidden")
-    musicProjectsContainerEl.removeClass("grid-container")
-
-    javascriptProjectsContainerEl.addClass("hidden")
-    javascriptProjectsContainerEl.removeClass("grid-container")
-
-    landingPagesContainerEl.removeClass("hidden")
-    landingPagesContainerEl.addClass("grid-container")
-})
-$(".javascript-projects").on("click", () => {
-    musicProjectsContainerEl.addClass("hidden")
-    musicProjectsContainerEl.removeClass("grid-container")
-
-    landingPagesContainerEl.addClass("hidden")
-    landingPagesContainerEl.removeClass("grid-container")
-
-    javascriptProjectsContainerEl.removeClass("hidden")
-    javascriptProjectsContainerEl.addClass("grid-container")
 })
 
 hamburgerIconEl.addEventListener("click", showMenu)
 
+$(".app").on("click", () => {
+    console.dir($(this))
+})
